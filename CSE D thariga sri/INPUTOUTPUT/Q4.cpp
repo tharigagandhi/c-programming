3b8742dc4475645546fbcff549fc10a11fb5c615
tharigagandhi <thariga2601008@gmail.com>
2025-08-28 23:32:38 -0700

    Add files via upload

---
+++ CSE D thariga sri/INPUTOUTPUT/Q4.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+float n;
+printf(" enter a number ");
+scanf("%f",&n);
+printf("%f",n);	
+return 0;	
+}
---
+++ CSE D thariga sri/INPUTOUTPUT/Q5.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+float n;
+printf(" enter a number ");
+scanf("%f",&n);
+printf("%.2f",n);	
+return 0;	
+}
